a8c290ca17d93505a63739d590ece173f47a9e9c
George G <ggogarten@gmail.com>
2016-06-17 13:29:41 -0400

    add new items and keyboard control

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -20,6 +20,9 @@ class FirstViewController: UIViewController, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +30,28 @@ class FirstViewController: UIViewController, UITableViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return toDoList.count
+        
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+        
+    }
 
+    override func viewDidAppear(animated: Bool) {
+        
+        toDoListTable.reloadData()
+        
+    }
+  
 }
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -34,6 +34,20 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
 
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        item.resignFirstResponder()
+        
+        return true
+        
+    }
+    
 }
 
